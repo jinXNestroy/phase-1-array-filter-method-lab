ae4897f61d5ac75809839cf86b032ccf0a4542b9
DanielJoel <danpersie53@gmail.com>
2024-04-03 12:54:49 +0300

    Filter method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+const findMatching = function (array, drivname) {
+    return array.filter(item => {
+        return item.toLowerCase() === drivname.toLowerCase();
+    });
+}
+
+const fuzzyMatch = function (array, string) {
+    return array.filter(item =>
+        item.toLowerCase().startsWith(string.toLowerCase()))
+
+}
+
+const matchName = function (array, string) {
+    return array.filter(item => item.name === string)
+
+}
